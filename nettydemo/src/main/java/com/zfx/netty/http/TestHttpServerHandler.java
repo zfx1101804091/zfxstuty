db2c2763d80e4ca05cc6430e7df3bd95dd8e911b
zfx1101804091 <loveu57929328>
2020-10-21 23:32:42 +0800

    netty-http-server编码

---
+++ nettydemo/src/main/java/com/zfx/netty/http/TestHttpServerHandler.java
@@ -0,0 +1,48 @@
+package com.zfx.netty.http;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 说明：
+ *  1\SimpleChannelInboundHandler 是 ChannelInboundHandlerAdapter
+ *  2\HttpObject 客户端与服务端相互通信的数据被封装为 HttpObject
+ */
+@Slf4j
+public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    /**
+     * 读取客户端数据
+     * @param ctx
+     * @param msg
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+
+        //判断msg是不是 HttpRequest请求
+        if(msg instanceof HttpRequest){
+            log.info("msg 类型 = ·「{}」",msg.getClass());
+            log.info("客户端地址: "+ctx.channel().remoteAddress().toString().substring(1));
+
+            //回复消息给客户端··「Http协议」
+            ByteBuf content = Unpooled.copiedBuffer("hello,我是服务器", CharsetUtil.UTF_8);
+
+            //构造一个http的响应（httpresponse）
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+
+            //将构建好的response返回
+            ctx.writeAndFlush(response);
+
+        }
+
+    }
+}
---
+++ nettydemo/src/main/java/com/zfx/netty/http/TestServer.java
@@ -0,0 +1,37 @@
+package com.zfx.netty.http;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+
+    public static void main(String[] args) {
+        /**
+         * 1、创建2个线程组 bossGroup workerGroup
+         * 2、bossGroup 只是处理连接请求，真正的和客户端业务处理，会交给wokerGroup完成
+         * 3、两个都是无限循环
+         * 4、bossGroup workerGroup含有的子线程（NioEventLoop）个数 == 默认实际CPU核数×2
+         */
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(1);
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture future = serverBootstrap.bind(6668).sync();
+            future.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
